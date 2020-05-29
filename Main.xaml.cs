a6750e197d5ac3335716c461ab6c5898bf5b5eb0
Yyering <29935648+Vickers-Zhu@users.noreply.github.com>
2020-05-29 17:51:24 +0200

    Initial commit

---
+++ Main.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Project
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Login.xaml", UriKind.Relative));
+        }
+
+        private void Dir_Add(object sender, RoutedEventArgs e)
+        {
+            this.TreeView.Items.Add(new TreeViewItem());
+        }
+
+        private void Pwd_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Img_Add(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
